94ee408a04af6c40ad112430388f9e69a52abd38
veer <veer.illinois@gmail.com>
2013-10-19 22:52:01 -0700

    initial commit

---
+++ src/com/veer/apps/twitterclient/TimelineActivity.java
@@ -0,0 +1,46 @@
+package com.veer.apps.twitterclient;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ListView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.veer.apps.twitterclient.models.Tweet;
+
+public class TimelineActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+		
+		TwitterClientApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler(){
+			@Override
+			public void onSuccess(JSONArray jsonTweets){
+				ArrayList<Tweet> tweets = Tweet.fromJson(jsonTweets);
+				
+				ListView lvTweets = (ListView)findViewById(R.id.lvTweets);
+				TweetsAdapter adapter = new TweetsAdapter(getBaseContext(), tweets);
+				lvTweets.setAdapter(adapter);
+				
+				Log.d("DEBUG", jsonTweets.toString());
+			}
+		});
+	}
+
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timeline, menu);
+		return true;
+	}
+
+}
